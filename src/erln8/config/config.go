7889dfbd1f24f34314b6a0b26286a0771b13e290
Dave Parfitt <dparfitt@basho.com>
2013-04-15 09:18:28 -0400

    wip

--- src/erln8/config/config.go
+++ src/erln8/config/config.go
@@ -1,5 +1,30 @@
 package config
 
-func DoStuff() int {
-  return 1
+type Erln8Config struct {
+  LinkDir string
 }
+
+type OTPDownloadSource struct {
+  Name string
+  URL string
+}
+
+type OTPVersion struct {
+  Name string
+  Major string
+  Minor string
+}
+
+type OTPCompilerFlagsSource struct {
+  Name string
+  URL string
+}
+
+type OTPCompileFlags struct {
+  Platform string
+  Tag string // release vs debug
+  Version string
+  Flags string
+}
+
+
